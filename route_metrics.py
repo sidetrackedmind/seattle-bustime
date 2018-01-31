f907a743cac38f9d3c1475570aea7f498cf562e4
sidetrackedmind <malnorba@gmail.com>
2018-01-31 10:29:49 -0800

    updtes to route_metrics

--- route_metrics.py
+++ route_metrics.py
@@ -226,7 +226,7 @@ def update_status_database(conn, route_dir):
 
     cur.execute("UPDATE route_metric_status "
                 "SET updated = 'true' "
-                    "WHERE route_dir  = '(%s)' ",(route_dir))
+                "WHERE route_dir  = '(%s)'",(route_dir))
     conn.commit()
 
 def percentile(n):
