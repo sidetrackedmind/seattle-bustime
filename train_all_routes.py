3ad06bc2555b1601e91372ee11cd74060c5894f4
sidetrackedmind <malnorba@gmail.com>
2018-02-04 12:27:24 -0800

    updated training all routes to account for different CPU values during multiprocessing

--- train_all_routes.py
+++ train_all_routes.py
@@ -24,7 +24,9 @@ def train_all_routes():
 
     route_dir_list = get_route_dir_list()
 
-    pool = multiprocessing.Pool(14)
+    n_pools = multiprocessing.cpu_count() - 2
+
+    pool = multiprocessing.Pool(n_pools)
     pool.map(train_one_route, route_dir_list)
 
 def train_one_route(route_dir):
@@ -40,69 +42,72 @@ def train_one_route(route_dir):
                             host=host,
                             port=port)
 
-    #mark route as "in progress"
-    route_in_progress(conn, route_dir)
+    #check if route has been cross validated
+    if cross_val_check(conn, route_dir):
 
-    best_alpha, best_depth = get_params_from_db(conn, route_dir)
+        #mark route as "in progress"
+        route_in_progress(conn, route_dir)
 
-    cur = conn.cursor()
+        best_alpha, best_depth = get_params_from_db(conn, route_dir)
 
-    query = '''
-            select distinct(route_id), direction_id
-            from route_info
-            where route_dir = '{}'
-            '''.format(route_dir)
+        cur = conn.cursor()
 
-    print('finding {}'.format(route_dir))
+        query = '''
+                select distinct(route_id), direction_id
+                from route_info
+                where route_dir = '{}'
+                '''.format(route_dir)
 
-    cur.execute(query)
-    query_list = cur.fetchall()
-    route_id = query_list[0][0]
-    direction = query_list[0][1]
-    pickle_path = build_filename(route_id, direction)
+        print('finding {}'.format(route_dir))
 
-    model_col_list = ['route_dir_stop','stop_sequence','month', 'day', 'hour','dow','delay']
+        cur.execute(query)
+        query_list = cur.fetchall()
+        route_id = query_list[0][0]
+        direction = query_list[0][1]
+        pickle_path = build_filename(route_id, direction)
 
-    select_string = column_list_to_string(model_col_list)
+        model_col_list = ['route_dir_stop','stop_sequence','month', 'day', 'hour','dow','delay']
 
-    query = '''
-            select {}
-            from updates
-            where route_id = {}
-            and direction_id = {}
-            '''.format(select_string, route_id, direction)
+        select_string = column_list_to_string(model_col_list)
 
-    print('getting historical route information for {}'.format(route_dir))
-    cur.execute(query)
-    query_list = cur.fetchall()
-    result = pd.DataFrame(query_list, columns=model_col_list)
-    y = (result.iloc[:,-1].values)/60
-    result = result.drop('delay', axis=1)
-    dummy_col = ['route_dir_stop','month', 'day', 'hour','dow']
-    result_dummies = pd.get_dummies(result,columns=dummy_col)
-    all_column_list = list(result_dummies.columns)
-    all_columns_str = column_list_to_string(all_column_list)
-    X = result_dummies.values
+        query = '''
+                select {}
+                from updates
+                where route_id = {}
+                and direction_id = {}
+                '''.format(select_string, route_id, direction)
 
-    gbr = GradientBoostingRegressor(loss='quantile',
-                                    n_estimators=3000,
-                                    learning_rate=0.015,
-                                    max_depth=best_depth,
-                                    subsample=0.5,
-                                    alpha=best_alpha,
-                                    random_state=128)
+        print('getting historical route information for {}'.format(route_dir))
+        cur.execute(query)
+        query_list = cur.fetchall()
+        result = pd.DataFrame(query_list, columns=model_col_list)
+        y = (result.iloc[:,-1].values)/60
+        result = result.drop('delay', axis=1)
+        dummy_col = ['route_dir_stop','month', 'day', 'hour','dow']
+        result_dummies = pd.get_dummies(result,columns=dummy_col)
+        all_column_list = list(result_dummies.columns)
+        all_columns_str = column_list_to_string(all_column_list)
+        X = result_dummies.values
 
-    print('starting model fit for {}_{}'.format(route_id, direction))
-    fit_model = gbr.fit(X, y)
-    print('model fit complete for {}_{}'.format(route_id, direction))
-    put_pickle_model(fit_model, pickle_path)
+        gbr = GradientBoostingRegressor(loss='quantile',
+                                        n_estimators=3500,
+                                        learning_rate=0.0075,
+                                        max_depth=best_depth,
+                                        subsample=0.5,
+                                        alpha=best_alpha,
+                                        random_state=128)
 
-    update_model_database(conn, all_columns_str, pickle_path, route_dir)
-    print('database updated for {}_{}'.format(route_id, direction))
-    mark_as_finished(conn, route_dir)
+        print('starting model fit for {}_{}'.format(route_id, direction))
+        fit_model = gbr.fit(X, y)
+        print('model fit complete for {}_{}'.format(route_id, direction))
+        put_pickle_model(fit_model, pickle_path)
 
-    cur.close()
-    conn.close()
+        update_model_database(conn, all_columns_str, pickle_path, route_dir)
+        print('database updated for {}_{}'.format(route_id, direction))
+        mark_as_finished(conn, route_dir)
+
+        cur.close()
+        conn.close()
 
 def get_route_dir_list():
     '''
@@ -179,6 +184,28 @@ def get_params_from_db(conn, route_dir):
     best_depth = query_list[0][1]
 
     return best_alpha, best_depth
+
+def cross_val_check(conn, route_dir):
+    '''
+    checking cross_val database
+    if the route_dir has been CV'd return True
+    else return False
+
+    OUTPUT
+    -------
+    True - if route_dir has been CV'd
+    False - if route_dir has not been CV'd
+    '''
+    cur = conn.cursor()
+
+    cur.execute("SELECT c_validated "
+                "FROM model_params "
+                "WHERE route_dir  = (%s) ",
+                [route_dir]
+                    )
+    cv_status = cur.fetchall()
+
+    return cv_status[0][0] == True
 
 
 def put_pickle_model(fit_model, filename):
