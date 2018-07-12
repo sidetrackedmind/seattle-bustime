529b33fcf333e8e6f66920aea05b3e28735c81b7
sidetrackedmind <malnorba@gmail.com>
2018-07-11 19:10:55 -0700

    took out matplotlib import from bus_shape_to_graph

--- bus_shape_to_graph.py
+++ bus_shape_to_graph.py
@@ -5,7 +5,6 @@ import os
 import re
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
 import geopandas as gpd
 from geopandas import GeoDataFrame
 from shapely.geometry import Point, LineString, MultiLineString
